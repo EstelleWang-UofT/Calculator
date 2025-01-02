541ecbb30f007a45befdaec8337398a8b231d70c
Estelle(Xiaoyi <xyestelle.wang@mail.utoronto.ca>
2025-01-02 12:12:11 -0500

    Can do: operand operator operand operator operand etc.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,7 @@
-﻿namespace Calculator
+﻿
+namespace Calculator
 {
-    partial class Form1
+    partial class FormCalculator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +29,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonNum1 = new Button();
+            buttonNum2 = new Button();
+            buttonNum3 = new Button();
+            buttonNum6 = new Button();
+            buttonNum5 = new Button();
+            buttonNum4 = new Button();
+            buttonDot = new Button();
+            buttonEquals = new Button();
+            buttonNum0 = new Button();
+            buttonNum9 = new Button();
+            buttonNum8 = new Button();
+            buttonNum7 = new Button();
+            buttonDivide = new Button();
+            buttonMultiply = new Button();
+            buttonMinus = new Button();
+            buttonPlus = new Button();
+            textBox = new TextBox();
+            buttonAC = new Button();
+            SuspendLayout();
+            // 
+            // buttonNum1
+            // 
+            buttonNum1.Location = new Point(90, 98);
+            buttonNum1.Name = "buttonNum1";
+            buttonNum1.Size = new Size(94, 29);
+            buttonNum1.TabIndex = 0;
+            buttonNum1.Text = "1";
+            buttonNum1.UseVisualStyleBackColor = true;
+            buttonNum1.Click += buttonNum1_Click;
+            // 
+            // buttonNum2
+            // 
+            buttonNum2.Location = new Point(190, 98);
+            buttonNum2.Name = "buttonNum2";
+            buttonNum2.Size = new Size(94, 29);
+            buttonNum2.TabIndex = 1;
+            buttonNum2.Text = "2";
+            buttonNum2.UseVisualStyleBackColor = true;
+            buttonNum2.Click += buttonNum2_Click;
+            // 
+            // buttonNum3
+            // 
+            buttonNum3.Location = new Point(290, 98);
+            buttonNum3.Name = "buttonNum3";
+            buttonNum3.Size = new Size(94, 29);
+            buttonNum3.TabIndex = 2;
+            buttonNum3.Text = "3";
+            buttonNum3.UseVisualStyleBackColor = true;
+            buttonNum3.Click += buttonNum3_Click;
+            // 
+            // buttonNum6
+            // 
+            buttonNum6.Location = new Point(290, 133);
+            buttonNum6.Name = "buttonNum6";
+            buttonNum6.Size = new Size(94, 29);
+            buttonNum6.TabIndex = 5;
+            buttonNum6.Text = "6";
+            buttonNum6.UseVisualStyleBackColor = true;
+            buttonNum6.Click += buttonNum6_Click;
+            // 
+            // buttonNum5
+            // 
+            buttonNum5.Location = new Point(190, 133);
+            buttonNum5.Name = "buttonNum5";
+            buttonNum5.Size = new Size(94, 29);
+            buttonNum5.TabIndex = 4;
+            buttonNum5.Text = "5";
+            buttonNum5.UseVisualStyleBackColor = true;
+            buttonNum5.Click += buttonNum5_Click;
+            // 
+            // buttonNum4
+            // 
+            buttonNum4.Location = new Point(90, 133);
+            buttonNum4.Name = "buttonNum4";
+            buttonNum4.Size = new Size(94, 29);
+            buttonNum4.TabIndex = 3;
+            buttonNum4.Text = "4";
+            buttonNum4.UseVisualStyleBackColor = true;
+            buttonNum4.Click += buttonNum4_Click;
+            // 
+            // buttonDot
+            // 
+            buttonDot.Location = new Point(190, 202);
+            buttonDot.Name = "buttonDot";
+            buttonDot.Size = new Size(94, 28);
+            buttonDot.TabIndex = 11;
+            buttonDot.Text = ".";
+            buttonDot.UseVisualStyleBackColor = true;
+            buttonDot.Click += buttonDot_Click;
+            // 
+            // buttonEquals
+            // 
+            buttonEquals.Location = new Point(290, 202);
+            buttonEquals.Name = "buttonEquals";
+            buttonEquals.Size = new Size(94, 28);
+            buttonEquals.TabIndex = 10;
+            buttonEquals.Text = "＝";
+            buttonEquals.UseVisualStyleBackColor = true;
+            buttonEquals.Click += buttonEquals_Click;
+            // 
+            // buttonNum0
+            // 
+            buttonNum0.Location = new Point(90, 203);
+            buttonNum0.Name = "buttonNum0";
+            buttonNum0.Size = new Size(94, 28);
+            buttonNum0.TabIndex = 9;
+            buttonNum0.Text = "0";
+            buttonNum0.UseVisualStyleBackColor = true;
+            buttonNum0.Click += buttonNum0_Click;
+            // 
+            // buttonNum9
+            // 
+            buttonNum9.Location = new Point(290, 168);
+            buttonNum9.Name = "buttonNum9";
+            buttonNum9.Size = new Size(94, 28);
+            buttonNum9.TabIndex = 8;
+            buttonNum9.Text = "9";
+            buttonNum9.UseVisualStyleBackColor = true;
+            buttonNum9.Click += buttonNum9_Click;
+            // 
+            // buttonNum8
+            // 
+            buttonNum8.Location = new Point(190, 168);
+            buttonNum8.Name = "buttonNum8";
+            buttonNum8.Size = new Size(94, 28);
+            buttonNum8.TabIndex = 7;
+            buttonNum8.Text = "8";
+            buttonNum8.UseVisualStyleBackColor = true;
+            buttonNum8.Click += buttonNum8_Click;
+            // 
+            // buttonNum7
+            // 
+            buttonNum7.Location = new Point(90, 168);
+            buttonNum7.Name = "buttonNum7";
+            buttonNum7.Size = new Size(94, 28);
+            buttonNum7.TabIndex = 6;
+            buttonNum7.Text = "7";
+            buttonNum7.UseVisualStyleBackColor = true;
+            buttonNum7.Click += buttonNum7_Click;
+            // 
+            // buttonDivide
+            // 
+            buttonDivide.Location = new Point(390, 202);
+            buttonDivide.Name = "buttonDivide";
+            buttonDivide.Size = new Size(94, 28);
+            buttonDivide.TabIndex = 15;
+            buttonDivide.Text = "/";
+            buttonDivide.UseVisualStyleBackColor = true;
+            buttonDivide.Click += buttonDivide_Click;
+            // 
+            // buttonMultiply
+            // 
+            buttonMultiply.Location = new Point(390, 168);
+            buttonMultiply.Name = "buttonMultiply";
+            buttonMultiply.Size = new Size(94, 28);
+            buttonMultiply.TabIndex = 14;
+            buttonMultiply.Text = "*";
+            buttonMultiply.UseVisualStyleBackColor = true;
+            buttonMultiply.Click += buttonMultiply_Click;
+            // 
+            // buttonMinus
+            // 
+            buttonMinus.Location = new Point(390, 133);
+            buttonMinus.Name = "buttonMinus";
+            buttonMinus.Size = new Size(94, 29);
+            buttonMinus.TabIndex = 13;
+            buttonMinus.Text = "-";
+            buttonMinus.UseVisualStyleBackColor = true;
+            buttonMinus.Click += buttonMinus_Click;
+            // 
+            // buttonPlus
+            // 
+            buttonPlus.Location = new Point(390, 98);
+            buttonPlus.Name = "buttonPlus";
+            buttonPlus.Size = new Size(94, 29);
+            buttonPlus.TabIndex = 12;
+            buttonPlus.Text = "+";
+            buttonPlus.UseVisualStyleBackColor = true;
+            buttonPlus.Click += buttonPlus_Click;
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(90, 65);
+            textBox.Name = "textBox";
+            textBox.ReadOnly = true;
+            textBox.RightToLeft = RightToLeft.No;
+            textBox.Size = new Size(394, 27);
+            textBox.TabIndex = 16;
+            textBox.TextAlign = HorizontalAlignment.Right;
+            // 
+            // buttonAC
+            // 
+            buttonAC.Location = new Point(90, 237);
+            buttonAC.Name = "buttonAC";
+            buttonAC.Size = new Size(94, 28);
+            buttonAC.TabIndex = 17;
+            buttonAC.Text = "AC";
+            buttonAC.UseVisualStyleBackColor = true;
+            buttonAC.Click += buttonAC_Click;
+            // 
+            // FormCalculator
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonAC);
+            Controls.Add(textBox);
+            Controls.Add(buttonDivide);
+            Controls.Add(buttonMultiply);
+            Controls.Add(buttonMinus);
+            Controls.Add(buttonPlus);
+            Controls.Add(buttonDot);
+            Controls.Add(buttonEquals);
+            Controls.Add(buttonNum0);
+            Controls.Add(buttonNum9);
+            Controls.Add(buttonNum8);
+            Controls.Add(buttonNum7);
+            Controls.Add(buttonNum6);
+            Controls.Add(buttonNum5);
+            Controls.Add(buttonNum4);
+            Controls.Add(buttonNum3);
+            Controls.Add(buttonNum2);
+            Controls.Add(buttonNum1);
+            Name = "FormCalculator";
+            Text = "Calculator";
+            Load += FormCalculator_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonNum1;
+        private Button buttonNum2;
+        private Button buttonNum3;
+        private Button buttonNum6;
+        private Button buttonNum5;
+        private Button buttonNum4;
+        private Button buttonDot;
+        private Button buttonEquals;
+        private Button buttonNum0;
+        private Button buttonNum9;
+        private Button buttonNum8;
+        private Button buttonNum7;
+        private Button buttonDivide;
+        private Button buttonMultiply;
+        private Button buttonMinus;
+        private Button buttonPlus;
+        private TextBox textBox;
+        private Button buttonAC;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,153 @@
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class FormCalculator : Form
     {
-        public Form1()
+
+        public FormCalculator()
         {
             InitializeComponent();
         }
+
+        public string valIn = "";
+        public string tempOp = "";
+        public string result = "";
+        public string prevIn = "";
+
+        private void numButtonClick(object sender, EventArgs e)
+        {
+            valIn = valIn + ((Button)(sender)).Text;
+            textBox.Text = valIn;
+            // textBox.show()
+        }
+
+        private void opButtonClick(object sender, EventArgs e)
+        {
+            if (result == "") { result = valIn; }
+            else { result = calculate().ToString(); }
+            textBox.Text = result;
+            valIn = "";
+            tempOp = ((Button)(sender)).Text;
+        }
+
+        private void buttonNum0_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum1_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum2_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum3_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum4_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum5_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum6_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum7_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum8_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonNum9_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonDot_Click(object sender, EventArgs e)
+        {
+            numButtonClick(sender, e);
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            result = Convert.ToString(calculate());
+            textBox.Text = result;
+            prevIn = valIn;
+            valIn = "";
+        }
+
+        private Single calculate()
+        {
+            Single num1 = 0;
+            if (result == "") { num1 = 0; }
+            else { num1 = Convert.ToSingle(result); }
+
+            Single num2 = 0;
+            if (valIn == "" && prevIn == "") { num2 = 0; }
+            else if (valIn == "" && prevIn != "") { num2 = Convert.ToSingle(prevIn); }
+            else { num2 = Convert.ToSingle(valIn); }
+
+            if (tempOp == "/" && num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0, please try again!");
+            }
+
+            Single res = 0;
+            if (tempOp == "+") { res = num1 + num2; }
+            else if (tempOp == "-") { res = num1 - num2; }
+            else if (tempOp == "*") { res = num1 * num2; }
+            else { res = num1 / num2; }
+            return res;
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            opButtonClick(sender, e);
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            opButtonClick(sender, e);
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            opButtonClick(sender, e);
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            opButtonClick(sender, e);
+        }
+
+        private void FormCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAC_Click(object sender, EventArgs e)
+        {
+            valIn = "";
+            tempOp = "";
+            result = "";
+            textBox.Text = result;
+        }
+
     }
 }
